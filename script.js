04c19d83704438a8222ca3c490f28e43998937fe
sanskard7 <sanskardongare7@gmail.com>
2026-04-03 15:43:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Simple scroll animation
-document.querySelectorAll('a').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href'))
-      .scrollIntoView({ behavior: 'smooth' });
-  });
-});
+let slides=document.querySelectorAll('.slide');
+let index=0;
+setInterval(()=>{
+  slides[index].classList.remove('active');
+  index=(index+1)%slides.length;
+  slides[index].classList.add('active');
+},3000);
